cf54beaa1abcc0e3699d550e351ccde26c4d86b0
Caio <caiocoelhoinfo@gmail.com>
2024-09-12 00:08:19 -0300

    Create if_elif_else.py

---
+++ if_elif_else.py
@@ -0,0 +1,14 @@
+#exemplo de if, elif e else
+
+#Atribuição de valores pelo usuário
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+
+
+#Condição if
+if a > b:
+    print("a é maior que b")
+elif a == b:
+    print("a é igual a b")
+else:
+    print("a é menor que b")
